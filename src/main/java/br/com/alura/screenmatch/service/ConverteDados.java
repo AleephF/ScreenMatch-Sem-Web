9fa95a4051cad1f6fbe9f2fdc181226f255acc73
AleephF <aleph22fernandes@hotmail.com>
2024-10-14 14:13:16 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/alura/screenmatch/service/ConverteDados.java
@@ -0,0 +1,11 @@
+package br.com.alura.screenmatch.service;
+
+public class ConverteDados implements IConverteDados {
+
+    private Object
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        return null;
+    }
+}
