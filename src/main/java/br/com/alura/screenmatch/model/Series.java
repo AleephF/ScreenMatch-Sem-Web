4475669e9c63e9734015d0fb2c9581634fba25e1
AleephF <aleph22fernandes@hotmail.com>
2024-10-29 14:22:49 -0300

    Atualização no código

---
+++ src/main/java/br/com/alura/screenmatch/model/Series.java
@@ -0,0 +1,125 @@
+package br.com.alura.screenmatch.model;
+
+import br.com.alura.screenmatch.service.ConsultaChatGPT;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalDouble;
+
+@Entity
+public class Series {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String titulo;
+    private Integer totalTemporadas;
+    private Double avaliacao;
+
+    @Enumerated(EnumType.STRING)
+    private Categoria genero;
+    private String atores;
+    private String poster;
+    private String sinopse;
+    @Transient
+    private List<Episodio> episodio = new ArrayList<>();
+
+    public Series() {}
+
+    public Series(DadosSerie dadosSerie){
+        this.titulo = dadosSerie.titulo();
+        this.totalTemporadas = dadosSerie.totalTemporadas();
+        this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0);
+        this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
+        this.atores = dadosSerie.atores();
+        this.poster = dadosSerie.poster();
+        this.sinopse = dadosSerie.sinopse();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Episodio> getEpisodio() {
+        return episodio;
+    }
+
+    public void setEpisodio(List<Episodio> episodio) {
+        this.episodio = episodio;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Integer getTotalTemporadas() {
+        return totalTemporadas;
+    }
+
+    public void setTotalTemporadas(Integer totalTemporadas) {
+        this.totalTemporadas = totalTemporadas;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    public String getAtores() {
+        return atores;
+    }
+
+    public void setAtores(String atores) {
+        this.atores = atores;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "Genero: " + genero +
+                "\nTitulo: " + titulo +
+                "\nTotalTemporadas: " + totalTemporadas +
+                "\nAvaliacao: " + avaliacao +
+                "\nAtores: " + atores +
+                "\nPoster: " + poster +
+                "\nSinopse: " + sinopse +
+                "\n*************************************************************************************";
+    }
+}
